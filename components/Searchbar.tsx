86ffbb14117292ef3d78c47db7ee378a9bd07b4b
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-10-28 11:53:53 +0530

    search queries cached

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -17,7 +17,8 @@ const StockSearchBar: React.FC<StockSearchBarProps> = ({
   onStockSelect,
 }) => {
   const [suggestions, setSuggestions] = useState<Stock[]>([]);
-  const stockData: Stock[] = [
+
+  const [stockData, setStockData] = useState<Stock[]>([
     { id: 1, name: 'Apple' },
     { id: 2, name: 'Microsoft' },
     { id: 3, name: 'Google' },
@@ -26,16 +27,25 @@ const StockSearchBar: React.FC<StockSearchBarProps> = ({
     { id: 6, name: 'Facebook' },
     { id: 7, name: 'Netflix' },
     { id: 8, name: 'NVIDIA' },
-  ];
+  ]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchStock(value);
 
-    if (value) {
+    if (value.trim()) {
       const filteredSuggestions = stockData.filter((stock) =>
-        stock.name.toLowerCase().startsWith(value.toLowerCase())
+        stock.name.toLowerCase().includes(value.toLowerCase())
       );
+
+      if (
+        !filteredSuggestions.some(
+          (stock) => stock.name.toLowerCase() === value.toLowerCase()
+        )
+      ) {
+        filteredSuggestions.push({ id: stockData.length + 1, name: value });
+      }
+
       setSuggestions(filteredSuggestions);
     } else {
       setSuggestions([]);
@@ -46,10 +56,21 @@ const StockSearchBar: React.FC<StockSearchBarProps> = ({
     setSearchStock(suggestion.name);
     setSuggestions([]);
     onStockSelect(suggestion.name);
+
+    if (
+      !stockData.some(
+        (stock) => stock.name.toLowerCase() === suggestion.name.toLowerCase()
+      )
+    ) {
+      setStockData([
+        ...stockData,
+        { id: stockData.length + 1, name: suggestion.name },
+      ]);
+    }
   };
 
   return (
-    <div className="relative flex-grow  w-full">
+    <div className="relative flex-grow w-full">
       <form
         onSubmit={(e) => e.preventDefault()}
         className="border border-white text-black rounded-md mb-2 md:mb-0 min-w-24 flex"
@@ -74,7 +95,7 @@ const StockSearchBar: React.FC<StockSearchBarProps> = ({
       </form>
 
       {suggestions.length > 0 && (
-        <ul className="absolute z-10 bg-white border border-gray-300 rounded-md w-full mt-1 max-h-60 overflow-y-auto">
+        <ul className="absolute z-10 bg-white border border-gray-300 rounded-md w-full mt-1 max-h-20 overflow-y-auto">
           {suggestions.map((suggestion) => (
             <li
               key={suggestion.id}
